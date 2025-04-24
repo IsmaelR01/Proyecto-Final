b0cd96782488962c2d2523841eba1ddca23aaf18
IsmaelR01 <ismaelrivasn@gmail.com>
2025-04-24 20:00:44 +0200

    he cambiado el color del fondo de los enlace y del submenu de productos

--- www/chaquetas.php
+++ www/chaquetas.php
@@ -14,7 +14,7 @@ $resultado = $consultaProducto->get_result();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Productos - Jerseys</title>
+    <title>Productos - Chaquetas</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/styles.css">
 </head>
